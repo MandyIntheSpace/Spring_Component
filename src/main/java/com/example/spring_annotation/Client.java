f8f111abfafddf298c73af9ed01429c9e3e467b1
MandyIntheSpace <timsinarewon@gmail.com>
2023-07-17 10:13:41 +0545

    regular commit

--- src/main/java/com/example/spring_annotation/Client.java
+++ src/main/java/com/example/spring_annotation/Client.java
@@ -1,15 +1,15 @@
 package com.example.spring_annotation;
 
 import org.apache.catalina.core.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Client {
     public static void main(String[] args) {
         System.out.println("We are here");
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("file.xml");
-        Student student = (Student) applicationContext.getBean("studentBean");
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(StudnetConfig.class);
+        Student student = (Student) applicationContext.getBean("studentBean", Student.class);
         student.setName("F1Soft Internationals");
         System.out.println("Student Name: " + student.getName());
-        System.out.println("The code finishes here");
     }
 }
---
+++ src/main/java/com/example/spring_annotation/StudnetConfig.java
@@ -0,0 +1,11 @@
+package com.example.spring_annotation;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan(basePackages = "com.example.spring_annotation")
+public class StudnetConfig {
+
+}
